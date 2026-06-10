84805cbe5e811320d144830b745fe6720985b275
qurbaniemrahov <qurbaniemrah@outlook.com>
2026-06-10 22:48:30 +0400

    updated

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,14 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('web.home');
-});
-
-Route::view('/login', 'auth.login')->name('login');
-Route::view('/register', 'auth.register')->name('register');
-
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::view('/', 'admin.dashboard')->name('dashboard');
-    Route::view('/login', 'admin.auth.login')->name('login');
-    Route::view('/register', 'admin.auth.register')->name('register');
+    return view('welcome');
 });
